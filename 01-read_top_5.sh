efe297d7096d8ffdf74098f3c36c0d596c4e9bf0
soumya816-us <soumyareddy816@gmail.com>
2025-01-16 23:50:42 -0500

    assignments

--- 01-read_top_5.sh
+++ 01-read_top_5.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+#(top 5 frequently  occurrence of each word in a file) 
 cat file2.txt | sed -r 's/[[:space:]]+/\n/g' | sed '/^$/d' | sort | uniq -c | sort -n | tail -n5
+
+# (top 1 frequently  occurrence of each word in a file) 
+cat file2.txt | sed -r 's/[[:space:]]+/\n/g' | sed '/^$/d' | sort | uniq -c | sort -n | tail -n1 
