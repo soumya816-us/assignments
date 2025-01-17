6c63ede10cff973d0e70158d63926a53cb324d4f
soumya816-us <soumyareddy816@gmail.com>
2025-01-17 00:41:05 -0500

    assignments

--- 04-replacement-words.sh
+++ 04-replacement-words.sh
@@ -3,12 +3,12 @@
 echo "the repalcement of a file is"
 # Backup Option: If you want to create a backup before modifying the file, use:
 
-sed -i.bak "s/\b$planet\b/$soumya/g" "$file"
+#sed -i.bak "s/\b$planet\b/$soumya/g" "$file"
 
 
-#sed -i '' "s/soumya/planet/g" file5.txt 
+sed -i '' "s/soumya/planet/g" file5.txt 
 
-#cat $file
+cat file5.txt
 
 # without backup
 #sed -i "s/$word_to_replace/$replacement_word/g" "$file"
