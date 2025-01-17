74770e848641ed7651ab1b40a8507d6253ef52e0
soumya816-us <soumyareddy816@gmail.com>
2025-01-16 23:58:30 -0500

    assignments

--- 02-transpose.sh
+++ 02-transpose.sh
@@ -2,22 +2,21 @@
 
 echo "the transpose of a file is"
 
-paste -s -d ' ' file3.txt
+# paste -s -d ' ' file3.txt
 
 
-# awk '
-# {
-#     for (i=1; i<=NF; i++) {
-#         a[i][NR] = $i  
-#     }
-# }
-# END {
-
-#     for (i=1; i<=NF; i++) {  
-#         for (j=1; j<=NR; j++) {  
-#             printf "%s ", a[i][j]  
-#         }
-#         print "" 
-#     }
-# }
-# ' file3.txt
+awk '
+{
+    for (i=1; i<=NF; i++) {
+        a[i, NR] = $i  # Store the i-th field (column) of row NR in array a
+    }
+}
+END {
+    for (i=1; i<=NF; i++) {  # Loop through the number of columns
+        for (j=1; j<=NR; j++) {  # Loop through the rows
+            printf "%s ", a[i,j]  # Print the transposed data
+        }
+        print ""  # Print a newline after each transposed row
+    }
+}
+' file3.txt
